f7bbe35c0f6c44bdd1280b5273e77872bfc26f34
Timothe <hegetimothe@gmail.com>
2024-08-01 14:00:16 +0200

    slider fini

--- src/app/component/composant/pages/home/hero/hero.component.ts
+++ src/app/component/composant/pages/home/hero/hero.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit, OnDestroy{
 
+  slides = [
+    { image: '../../../../assets/images/swiper/swiper1.jpg', caption: 'Caption 1' },
+    { image: '../../../../assets/images/swiper/swiper2.jpg', caption: 'Caption 2' },
+    { image: '../../../../assets/images/swiper/swiper3.jpg', caption: 'Caption 3' }
+  ];
+  currentSlide = 0;
+  intervalId: any;
+
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
+  startAutoSlide() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
+  stopAutoSlide() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide > 0) ? this.currentSlide - 1 : this.slides.length - 1;
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide < this.slides.length - 1) ? this.currentSlide + 1 : 0;
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
+  }
 }
