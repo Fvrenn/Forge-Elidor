59247bf6e0e1c536f31cddd865998dd96408e99a
Timothe <hegetimothe@gmail.com>
2024-08-09 23:11:21 +0200

    page produit fini

--- src/app/component/composant/pages/produit/produit-grid/produit-grid.component.ts
+++ src/app/component/composant/pages/produit/produit-grid/produit-grid.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './produit-grid.component.scss'
 })
 export class ProduitGridComponent {
+  showBasket(event: MouseEvent): void {
+    const element = (event.currentTarget as HTMLElement).querySelector('.add-basket');
+    if (element) {
+      element.classList.add('show');
+    }
+  }
 
+  hideBasket(event: MouseEvent): void {
+    const element = (event.currentTarget as HTMLElement).querySelector('.add-basket');
+    if (element) {
+      element.classList.remove('show');
+    }
+  }
 }
