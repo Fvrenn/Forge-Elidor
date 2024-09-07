3403586be822bd5e996f34ca77235e45ac588ce5
Timothe <hegetimothe@gmail.com>
2024-09-07 22:34:31 +0200

    ajout du changement d'image

--- src/app/component/pages/detail-produit/detail-produit.component.ts
+++ src/app/component/pages/detail-produit/detail-produit.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './detail-produit.component.html',
   styleUrl: './detail-produit.component.scss'
 })
-export class DetailProduitComponent {
 
+export class DetailProduitComponent {
+  imagePrincipale: string = '../../../../assets/images/couteau/couteau_1.jpg';
+  
+  images: string[] = [
+    '../../../../assets/images/couteau/couteau_1.jpg',
+    '../../../../assets/images/couteau/couteau_9.jpg',
+    '../../../../assets/images/couteau/couteau_4.jpg'
+  ];
+
+  changerImagePrincipale(nouvelleImage: string) {
+    this.imagePrincipale = nouvelleImage;
+  }
 }
