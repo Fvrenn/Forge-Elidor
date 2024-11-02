5446053cffbff40d475f4760e8bd0b6932f68048
Timothe <hegetimothe@gmail.com>
2024-11-02 20:33:24 +0100

    filtre fonctionnele pour la nav et bone url

--- src/app/component/composant/pages/produit/produit-grid/produit-grid.component.ts
+++ src/app/component/composant/pages/produit/produit-grid/produit-grid.component.ts
@@ -1,41 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { KnifeService } from '../../../../../services/knife/knife.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-produit-grid',
   templateUrl: './produit-grid.component.html',
   styleUrl: './produit-grid.component.scss'
 })
-export class ProduitGridComponent implements OnInit {
+export class ProduitGridComponent implements OnInit, OnDestroy {
   knives: any[] = [];
   filteredKnives: any[] = [];
   selectedCategory: string = '';
+  private routerSubscription!: Subscription; // Utiliser l'opérateur !
 
-  constructor(private knifeService: KnifeService, private route: ActivatedRoute) {}
+  constructor(private knifeService: KnifeService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     this.loadKnives();
-    this.route.queryParams.subscribe(params => {
-      if (params['category']) {
-        this.selectedCategory = params['category'];
-        this.filterKnives(this.selectedCategory);
-      } else {
-        this.filterKnives('');
-      }
+    this.routerSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.applyFilterFromUrl();
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
   }
 
   loadKnives(): void {
     this.knifeService.getKnives().subscribe(
       (data: any[]) => {
         this.knives = data;
-        this.filterKnives(this.selectedCategory);
+        this.applyFilterFromUrl();
       },
       (error: any) => {
         console.error('Failed to fetch knives', error);
       }
     );
+  }
+
+  applyFilterFromUrl(): void {
+    const url = this.router.url;
+    const category = url.split('/').pop();
+    if (category && category !== 'produit') {
+      this.filterKnives(category);
+    } else {
+      this.filterKnives('');
+    }
   }
 
   filterKnives(category: string): void {
